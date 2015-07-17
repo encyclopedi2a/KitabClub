e978483a204ef922b75a43e5751a7e9d40cfc094
encyclopedi2a <gppantha2014@gmail.com>
2015-07-17 13:48:20 +0545

    initial commit

--- app/src/main/java/com/sunbi/organisatiom/activity/kitabclub/MainActivity.java
+++ app/src/main/java/com/sunbi/organisatiom/activity/kitabclub/MainActivity.java
@@ -2,12 +2,26 @@ package com.sunbi.organisatiom.activity.kitabclub;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView username;
+    private TextView password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (TextView) findViewById(R.id.username);
+        username.setOnClickListener(this);
+        password = (TextView) findViewById(R.id.password);
+        password.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        username.setBackgroundResource(R.drawable.selector_state);
+        password.setBackgroundResource(R.drawable.selector_state);
     }
 }
